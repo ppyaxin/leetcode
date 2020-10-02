59e653bad431a3b816e0fb144f39b07b0166b4f7
ppyaxin <pyx518@qq.com>
2020-10-02 23:28:58 +0800

    动态规划略复杂解法

--- problem62.js
+++ problem62.js
@@ -15,32 +15,63 @@ var uniquePaths = function (m, n) {
         }
         for (let i = 0; i < 2; i++) { //i=0向右走，i=1向下走
             if (i == 0 && right > 1) {
-                getPath(right-1, down)
+                getPath(right - 1, down)
             }
             if (i == 1 && down > 1) {
-                getPath(right, down-1)
+                getPath(right, down - 1)
             }
         }
     }
     return count;
 };
 
-//动态规划题解
-var uniquePaths2 = function(m, n) {
+//动态规划
+var uniquePaths3 = function (m, n) {
     // 动态规划
     // 时间复杂度 O(n^2)
     // 空间复杂度 O(n^2)
-    let dp = Array.from({length:m}, () => Array.from({length:n}, () => 0))
+    let dp = Array.from(
+        { length: m },
+        () => Array.from(
+            { length: n },
+            () => 0
+        )
+    )
     // 第一列的走法只有一种
-    for (let i = 0; i < m; i++){
+    for (let i = 0; i < m; i++) {
         dp[i][0] = 1;
     }
     // 第一行的走法只有一种
-    for (let j = 0; j < n; j++){
+    for (let j = 0; j < n; j++) {
         dp[0][j] = 1;
     }
-    for (let i = 1; i < m; i++){
-        for (let j = 1; j < n; j++){
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            for (let k = 0; k <= i; k++) {
+                dp[i][j] += dp[k][j - 1]
+            }
+            // console.log("第"+(i+1)+"行第"+(j+1)+"列的值为："+dp[i][j])
+        }
+    }
+    return dp[m - 1][n - 1]
+};
+
+//动态规划题解
+var uniquePaths2 = function (m, n) {
+    // 动态规划
+    // 时间复杂度 O(n^2)
+    // 空间复杂度 O(n^2)
+    let dp = Array.from({ length: m }, () => Array.from({ length: n }, () => 0))
+    // 第一列的走法只有一种
+    for (let i = 0; i < m; i++) {
+        dp[i][0] = 1;
+    }
+    // 第一行的走法只有一种
+    for (let j = 0; j < n; j++) {
+        dp[0][j] = 1;
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
             // 当前位置是从左边或上边走过来的
             dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
@@ -49,5 +80,6 @@ var uniquePaths2 = function(m, n) {
 };
 
 
-console.log(uniquePaths(51, 9))
-console.log(uniquePaths2(51, 9))
+console.log(uniquePaths(5, 6))
+console.log(uniquePaths2(5, 6))
+console.log(uniquePaths3(5, 6))
