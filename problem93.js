d728e83be707624051cdc1b360f41ca82bb8d068
panyaxin <pyx518@qq.com>
2020-10-10 12:32:04 +0800

    80题提交和本地输出结果不同

--- problem93.js
+++ problem93.js
@@ -2,26 +2,6 @@
  * @param {string} s
  * @return {string[]}
  */
-//复原ip地址
-// var restoreIpAddresses = function(s) {
-//     let length=s.length
-//     if(length>12) return
-//     let results=[]
-//     let result=""
-//     function getResult(index,leftstring,number){//index是第几个字节，number是一个字节有几个数字
-//         let minNumber=Math.min(leftstring-(4-index)*3,1) 
-//         let maxNumber=Math.min(leftstring-(4-index)*1,3) 
-//         for(let i=minNumber;i<=maxNumber;i++){
-//             if(leftstring.charAt(i)==0){
-//                 continue
-//             }else{
-//                 let temp=leftstring.substr(0,i)
-//                 leftstring=leftstring.substr(i)
-//                 result=result+"."+temp
-//             }
-//         }
-//     }
-// };
 
 // join() 方法用于把数组中的所有元素放入一个字符串。
 // 元素是通过指定的分隔符进行分隔的。
@@ -31,50 +11,29 @@ var restoreIpAddresses = function (s) {
     let results = []
     let result = []
     function getResult(leftstring, index) {
+        function resultPush(i) {
+            result.push(leftstring.slice(0, i))
+            getResult(leftstring.substr(i), index + 1)
+            result.pop()
+        }
         if (index > 4) {
-            console.log("当前result为："+result)
-            if(leftstring.length==0){
+            if (leftstring.length == 0) {
                 results.push(result.join("."))
             }
             return
         }
-        // let minNumber = Math.min(leftstring - (4 - index) * 3, 1)
-        // let maxNumber = Math.min(leftstring - (4 - index) * 1, 3)
-        for (let i = 1; i <= 3; i++) {
-            if(leftstring.length==0){
-                break
-            }
-            let num=parseInt(leftstring.slice(0,i))
-            console.log("当前数字为："+num)
-            if(i==1){
-                result.push(num)
-                console.log("当前result为："+result)
-                getResult(leftstring.substr(i),index+1)
-                result.pop()
-            }
-            if(i==2){
-               if(num<10){
-                   continue
-               }else{
-                   result.push(num)
-                   console.log("当前result为："+result)
-                   getResult(leftstring.substr(i),index+1)
-                   result.pop()
-               }
-            }
-            if(i==3){
-                if(num<100||num>255){
-                    continue
-                }else{
-                    result.push(num)
-                    console.log("当前result为："+result)
-                    getResult(leftstring.substr(i),index+1)
-                    result.pop() 
-                }
-            }
+        if (leftstring.length == 0) return
+        resultPush(1)
+        if (leftstring.slice(0, 2) >= 10) {
+            resultPush(2)
         }
+        if (leftstring.slice(0, 3) >= 100 && leftstring.slice(0, 3) <= 255) {
+            resultPush(3)
+        }
+
     }
-    getResult(s,1)
+    getResult(s, 1)
     return results
 };
-console.log(restoreIpAddresses("102131342341023"))
+
+console.log(restoreIpAddresses("1111"))
